eed01d4c77e5efdcc17b580308ee426ea7d2da01
taewoo <ch05yusin@cau.ac.kr>
2025-12-09 14:02:01 +0900

    plus Option

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ second = input("Enter the second number: ")
 num1 = int(first)
 num2 = int(second)
 
-result = num1 + num2
+sun_result = num1 + num2
+sub_ result = num1 - num2
+mul_result = num1 * num2
 
-print("Sum: ", result)
+print ("Sum: ",sum_result)
+print ("Diffrence: ",sub_result)
+print ("Product: ", mul_result)
+
 print("수고했어 태우야")
